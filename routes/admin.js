b549669a5d36f1d67a45172e491ee5e0af64e9c5
Jaeho Cho <real0131@gmail.com>
2016-08-15 23:36:50 +0900

    data

--- routes/admin.js
+++ routes/admin.js
@@ -3,9 +3,29 @@
  */
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+
+
+
+var client = mysql.createConnection({
+    user : key.user,
+    password: key.password
+});
+
+client.query(key.db);
 
 router.get('/', function(req, res, next) {
     //res.send('admin');
-    res.render('admin', { title: 'Express' });
+    client.query(key.table,function (error, results) {
+        if (error)
+        {
+            console.log(error);
+        }else{
+            console.log('---------db connected');
+            console.log(results);
+            res.render('admin', { title: 'Express', data : results });
+        }
+    });
 });
+
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+
+
+
+var client = mysql.createConnection({
+  user : key.user,
+  password: key.password
+});
+
+client.query(key.db);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  client.query(key.table,function (error, results) {
+    if (error) {
+      console.log(error);
+    }else {
+      res.render('index', { title: 'Express', data : results });
+    }
+    });
 });
 
 router.get('/test', function(req, res, next) {
-  res.render('post', { title: 'Express' });
+  res.render('post', { title: 'Express'});
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Express'});
 });
 module.exports = router;
